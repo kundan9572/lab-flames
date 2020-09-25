285693142a52971cb7f7d2921586f704786d5915
kundan kumar <kd.kundan9572@gmail.com>
2020-09-25 23:24:13 +0530

    Completed

--- src/service/FlamesCheckService.java
+++ src/service/FlamesCheckService.java
@@ -1,40 +1,92 @@
 package service;
 
 // Create a class called FlamesCheckService
-public class FlamesCheckService{
-	
-	// FlamesCheckService has a method findFlames to find the flames between two names
-	// char findFlames(String name1, String name2) takes two strings as arguments
-	public char findFlames(String name1 , String name2) {
-		// Your task is to calculate the flames value and return the corresponding character as output 
-		int count=0;
-		for(int i=0;i<20;i++) {
-			if(name1.charAt(i) != name2.charAt(i)) {
-				count++;
+// FlamesCheckService has a method findFlames to find the flames between two names
+// char findFlames(String name1, String name2) takes two strings as arguments
+// Your task is to calculate the flames value and return the corresponding character as output 
+// You must return only the following values ['f','l','a','m','e','s']
+// change the return value at the end of the method corresponding to your return value
+
+import service.FlamesCheckService;
+
+// Create a class called FlamesCheckService
+public class FlamesCheckService {
+	public char findFlames(String name1, String name2) {
+		StringBuffer str1 = new StringBuffer(name1);
+		StringBuffer str2 = new StringBuffer(name2);
+		int len1 = str1.length();
+		int len2 = str2.length();
+		P: for (int i = 0; i < len1; i++)// label
+		{
+			char a = str1.charAt(i);
+
+			for (int j = 0; j < len2; j++) {
+				char b = str2.charAt(j);
+				if (a == b) {
+
+					str1.deleteCharAt(i);
+					str2.deleteCharAt(j);
+					len1 = str1.length();
+					len2 = str2.length();
+					i = 0;
+					j = 0;
+
+				}
+			}
+
+		}
+
+		int len = len1 + len2;
+		StringBuffer str3 = new StringBuffer("flames");
+		String str4 = new String();
+
+		P: for (int i = 0; i < 5; i++) {
+			int c = -1, d = 0, p = 0;
+
+			for (int j = 1; j <= len; j++) {
+				c++;
+				d++;
+
+				if (c > str3.length() - 1) {
+					char e = str3.charAt(p);
+					if (d == len) {
+						str3.deleteCharAt(p);
+
+						str4 = str3.substring(p, str3.length());
+
+						str3.delete(p, str3.length());
+
+						str3.insert(0, str4);
+
+						break;
+					} else {
+						p++;
+						if (p == str3.length()) {
+							p = 0;
+						}
+					}
+				} else {
+					char e = str3.charAt(c);
+					if (d == len) {
+						str3.deleteCharAt(c);
+
+						str4 = str3.substring(c, str3.length());
+
+						str3.delete(c, str3.length());
+
+						str3.insert(0, str4);
+
+						break;
+					}
+
+				}
+
 			}
 		}
-		
-		// You must return only the following values ['f','l','a','m','e','s']
-		for(int i=1;i<=count;i++) {
-			if(i==1)
-				return 'f';
-			else if(i==2)
-				return 'l';
-			else if(i==3)
-				return 'a';
-			else if(i==4)
-				return 'm';
-			else if(i==5)
-				return 'e';
-			else if(i==6)
-				return 's';
-			
-		}
-		// change the return value at the end of the method corresponding to your return value
 
-		return 'f';
-		
+		char result = str3.charAt(0);
+		return result;
+
 	}
-	
-	
+
 }
